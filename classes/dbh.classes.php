140e05fc4151c18e839c6b5fd2e9988d7d6e16b1
Joseph Martin Garado <garadojosephmartin98@gmail.com>
2023-03-05 14:08:45 +0800

    initial project

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -4,18 +4,18 @@ class Dbh
 {
     protected function connect()
     {
-    try
-    {
-        $username = "root";
-        $password = "";
-        $dbh = new PDO('mysqli:host=localhost;dbname=ooplogin', $username, $password);
-        return $dbh;
-    }
-    catch(PDOException $e)
-    {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
+        try
+        {
+            $username = "root";
+            $password = "";
+            $dbh = new PDO('mysqli:host=localhost;dbname=ooplogin', $username, $password);
+            return $dbh;
+        }
+        catch(PDOException $e)
+        {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 
 }
